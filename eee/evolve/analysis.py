89fc1559482ba8a44a6aa1643755030eefde8a4f
Michael Harms <harmsm@gmail.com>
2023-08-09 12:20:30 -0700

    analysis code matches refactor

--- eee/evolve/analysis.py
+++ eee/evolve/analysis.py
@@ -7,14 +7,14 @@ import pandas as pd
 from tqdm.auto import tqdm
 
 
-def get_genotype_frequencies(results,cutoff=50):
+def get_genotype_frequencies(generations,cutoff=50):
     """
     Extract genotype frequencies over simulation. 
     
     Parameters
     ----------
-    results : EvolutionResults
-        results returned by a Wright-Fisher simulation
+    generations : list
+        list of dicts holding results returned by a Wright-Fisher simulation
     cutoff : int, default=50
         drop genotypes seen fewer than cutoff times over the simulation
     
@@ -27,7 +27,7 @@ def get_genotype_frequencies(results,cutoff=50):
 
     max_counts = {}
     population_size = None
-    for g in tqdm(results.generations):
+    for g in tqdm(generations):
 
         if population_size is None:
             population_size = 0
@@ -48,10 +48,10 @@ def get_genotype_frequencies(results,cutoff=50):
     
     out_dict = {}
     for k in keys_to_keep:
-        out_dict[k] = np.zeros(len(results.generations),dtype=float)
-        for i in range(len(results.generations)):
+        out_dict[k] = np.zeros(len(generations),dtype=float)
+        for i in range(len(generations)):
             try:
-                out_dict[k][i] = results.generations[i][k]/population_size
+                out_dict[k][i] = generations[i][k]/population_size
             except KeyError:
                 pass
         
